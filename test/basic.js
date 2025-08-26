1e7081b732754f4f72d55b8345f8bc24ba4d0e65
Mathias Buus <mathiasbuus@gmail.com>
2025-08-26 19:35:05 +0200

    fuzz test

--- test/basic.js
+++ test/basic.js
@@ -15,3 +15,84 @@ test('basic', async function (t) {
   t.alike((await db.get(Buffer.from('hej'))).value, Buffer.from('verden'))
   t.alike((await db.get(Buffer.from('hello'))).value, Buffer.from('world'))
 })
+
+test('100 keys', async function (t) {
+  const db = await create(t)
+
+  const expected = []
+  for (let i = 0; i < 100; i++) {
+    const w = db.write()
+    const k = Buffer.from('' + i)
+    expected.push(k)
+    w.tryPut(k, k)
+    await w.flush()
+  }
+
+  expected.sort(Buffer.compare)
+  const actual = []
+
+  for await (const data of db.createReadStream()) {
+    actual.push(data.key)
+  }
+
+  t.alike(actual, expected)
+})
+
+test('1000 keys in 10 batches', async function (t) {
+  const db = await create(t)
+
+  const expected = []
+
+  let n = 0
+
+  for (let i = 0; i < 10; i++) {
+    const w = db.write()
+    for (let j = 0; j < 100; j++) {
+      const k = Buffer.from('' + (n++))
+      expected.push(k)
+      w.tryPut(k, k)
+    }
+    await w.flush()
+  }
+
+  expected.sort(Buffer.compare)
+  const actual = []
+
+  for await (const data of db.createReadStream()) {
+    actual.push(data.key)
+  }
+
+  t.alike(actual, expected)
+})
+
+test('basic fuzz (2k rounds)', async function (t) {
+  const db = await create(t)
+
+  const expected = new Map()
+
+  for (let i = 0; i < 2000; i++) {
+    const n = (Math.random() * 10) | 0
+    const w = db.write()
+    for (let j = 0; j < n; j++) {
+      const put = Math.random() < 0.8
+      const k = Buffer.from('' + ((Math.random() * 10_000) | 0))
+      if (put) {
+        expected.set(k.toString(), k)
+        w.tryPut(k, k)
+      } else {
+        expected.delete(k.toString())
+        w.tryDelete(k)
+      }
+    }
+    await w.flush()
+  }
+
+  const sorted = [...expected.values()].sort(Buffer.compare)
+  const actual = []
+
+  for await (const data of db.createReadStream()) {
+    actual.push(data.key)
+  }
+
+  t.alike(actual, sorted)
+})
