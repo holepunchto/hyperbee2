eef09dae1fd20626360150c52d5e78917f91d1e8
Mathias Buus <mathiasbuus@gmail.com>
2026-01-09 22:24:11 +0100

    trim example

--- example.mjs
+++ example.mjs
@@ -18,28 +18,3 @@ if (b.core.length === 0) {
 for await (const data of b.createReadStream()) {
   console.log(data.key, '-->', data.value)
 }
-
-// const c = new Hyperbee(b.store, { core: b.store.get({ name: 'bee2' }) })
-// await c.ready()
-
-// if (c.core.length === 0) {
-//   const w = c.write({ length: b.core.length, key: b.core.key })
-
-//   w.tryPut(Buffer.from('yo'), Buffer.from('yo'))
-//   await w.flush()
-// }
-
-// const s = c.checkout(b.head())
-
-// console.log(!!(await c.get(Buffer.from('yo'))))
-// console.log(!!(await s.get(Buffer.from('yo'))))
-
-// for await (const data of c.createReadStream()) {
-//   console.log(data.key.toString())
-// }
-
-// console.log()
-
-// for await (const data of s.createReadStream()) {
-//   console.log(data.key.toString())
-// }
