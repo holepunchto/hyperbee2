165000d89a34799b880263cb6e674b45007f7d48
Dominic Cassidy <dominic@cassidy.casa>
2026-03-25 14:25:03 +0000

    fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Hyperbee extends EventEmitter {
     const key = opts.key || null
 
     const getEncryptionProvider = (k) =>
-      opts.encryption || (opts.getEncryptionProvider ? opts.getEncryption(k) : null)
+      opts.encryption || (opts.getEncryption ? opts.getEncryption(k) : null)
 
     const encryption = getEncryptionProvider(key)
     const core = key ? store.get({ key: key, encryption }) : store.get({ name: 'bee', encryption })
