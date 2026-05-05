eb0838717305dc3888bb647c89c95487f69bc78d
Caolan McMahon <caolan@caolan.uk>
2026-05-05 16:54:25 +0100

    Document reducer methods

--- example/total-reducer.mjs
+++ example/total-reducer.mjs
@@ -8,7 +8,7 @@ const total = (values, rereduce) => {
     if (rereduce) {
       total += v
     } else {
-      total += Number(v.value.toString().substring(1))
+      total += Number(v.value.toString())
     }
   }
   return total
@@ -26,7 +26,7 @@ if (b.core.length === 0) {
   const w = b.write()
 
   for (let i = 0; i < 1_000_000; i++) {
-    w.tryPut(Buffer.from('#' + i), Buffer.from('#' + i))
+    w.tryPut(Buffer.from('' + i), Buffer.from('' + i))
   }
 
   await w.flush()
@@ -35,7 +35,7 @@ if (b.core.length === 0) {
 
   const w = b.write()
 
-  w.tryPut(Buffer.from('#500000' + Math.random()), Buffer.from('#500000' + Math.random()))
+  w.tryPut(Buffer.from('500000' + Math.random()), Buffer.from('500000' + Math.random()))
 
   await w.flush(reducers)
 }
@@ -51,7 +51,7 @@ await timeIt(async () => await b.reduce('total', total))
 
 console.log('Time query of total reducer over range')
 await timeIt(
-  async () => await b.reduceRange('total', total, Buffer.from('#250000'), Buffer.from('#750000'))
+  async () => await b.reduceRange('total', total, Buffer.from('250000'), Buffer.from('750000'))
 )
 
 console.log('Time query of temporary reducer')
